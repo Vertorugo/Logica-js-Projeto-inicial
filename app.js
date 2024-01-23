fd35b71be69449fadbb73cefa6da13c1724abb0d
Vertorugo <victor.ribeiro96@hotmail.com>
2024-01-23 20:01:51 -0300

    Função variavel e maior e menor

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-alert('Bem vindo ao jogo do Número Secreto');
+alert('Bem-vindo ao jogo do Número Secreto');
 let numeroSecreto = 7;
-let chute = prompt('Escolha um número entre 1 e 10')
+let chute = parseInt(prompt('Escolha um número entre 1 e 10'));
 
-if (chute == numeroSecreto) {
-    console.log('Isso Aí! Você acertou o CR7')
+while (chute !== numeroSecreto) {
+    if (chute > numeroSecreto) {
+        chute = parseInt(prompt('Escolha um número menor. Tente novamente:'));
+    } else {
+        chute = parseInt(prompt('Escolha um número maior. Tente novamente:'));
+    }
 }
+alert('Isso aí! Você acertou o CR7');
