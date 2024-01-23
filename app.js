f1215d879d3bf545aa9e17c4da1680e8b360bcc6
Vertorugo <victor.ribeiro96@hotmail.com>
2024-01-23 19:23:24 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert('Bem vindo ao jogo do Número Secreto');
+let numeroSecreto = 7;
+let chute = prompt('Escolha um número entre 1 e 10')
+
+if (chute == numeroSecreto) {
+    console.log('Isso Aí! Você acertou o CR7')
+}
